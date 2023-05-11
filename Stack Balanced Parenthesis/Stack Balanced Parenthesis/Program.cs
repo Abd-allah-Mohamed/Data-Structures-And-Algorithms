956c52cbf1fc0318d157ef7cf024a965b3d69080
Abdallah Mohamed <82576029+Abd-allah-Mohamed@users.noreply.github.com>
2023-05-11 10:44:18 +0300

    Add files via upload

---
+++ Stack Balanced Parenthesis/Stack Balanced Parenthesis/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Stack_Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string checkBrackets = "({})";
+            Console.WriteLine(isBalanced(checkBrackets));
+        }
+        public static bool isBalanced(string brackets)
+        {
+            StackLinkedList br = new StackLinkedList();
+            foreach (char ch in brackets.ToCharArray())
+            {
+                switch (ch)
+                {
+                    case '{':
+                    case '(':
+                    case '[':
+                        br.push(ch);
+                        break;
+
+                    case '}':
+                        if (br.pop() != '{')
+                        {
+                            return false;
+                        }
+                        break;
+
+                    case ')':
+                        if (br.pop() != '(')
+                        {
+                            return false;
+                        }
+                        break;
+
+                    case ']':
+                        if (br.pop() != '[')
+                        {
+                            return false;
+                        }
+                        break;
+                }
+            }
+            return br.size() == 0;
+        }
+    }
+    class Node
+    {
+        public Node next;
+        public char data;
+        public Node(char data)
+        {
+            this.data = data;
+        }
+    }
+    class StackLinkedList
+    {
+        Node top;
+        public bool isEmpty()
+        {
+            if (top == null)
+                return true;
+            else
+                return false;
+        }
+        public int size()
+        {
+            int count = 0;
+            Node p = top;
+            while (p != null)
+            {
+                p = p.next;
+                count++;
+            }
+            return count;
+        }
+        public void push(char data)
+        {
+            Node temp = new Node(data);
+            temp.next = top;
+            top = temp;
+        }
+        public int pop()
+        {
+            if (isEmpty())
+            {
+                return int.MinValue;
+            }
+            char temp = top.data;
+            top = top.next;
+            return temp;
+        }
+    }
+}
---
+++ Stack Reversing Word/Stack Reversing Word/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Stack_Reversing_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word to reverse : ");
+            string word = Console.ReadLine();
+            reverseWord(word);
+        }
+        static void reverseWord(string word)
+        {
+            StackArray s = new StackArray(word.Length);
+            char [] ch = word.ToCharArray();
+            for(int i = 0; i < word.Length; i++)
+            {
+                s.push(ch[i]);
+            }
+            while(!s.isEmpty())
+            {
+                Console.Write(s.pop());
+            }
+        }
+    }
+    class StackArray
+    {
+        int top;
+        char[] dataArray;
+        public StackArray(int size)
+        {
+            dataArray = new char[size];
+            top = -1;
+        }
+        public bool isEmpty()
+        {
+            if (top == -1)
+                return true;
+            else
+                return false;
+        }
+        public bool isFull()
+        {
+            if (top == dataArray.Length - 1)
+                return true;
+            else
+                return false;
+        }
+        public void push(char data)
+        {
+            if (isFull())
+                Console.WriteLine("Stack OverFlow");
+            else
+                dataArray[++top] = data;
+        }
+        public char pop()
+        {
+            if(isEmpty())
+            {
+                Console.WriteLine("Stack UnderFlow");
+                return char.MinValue;
+            }
+            else
+                return dataArray[top--];
+        }
+    }
+}

24b87187553a2e8cd6a469b316c563a089662fdf
Abdallah Mohamed <82576029+Abd-allah-Mohamed@users.noreply.github.com>
2023-05-11 10:43:14 +0300

    Add files via upload

---
+++ Stack Using Array/Stack Using Array/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Stack_Using_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackArray S1 = new StackArray(5);
+            Console.WriteLine(S1.isEmpty()); //empty returns true
+            S1.push(1); //1
+            S1.push(2); //2 1
+            S1.push(3); //3 2 1
+            S1.push(4); //4 3 2 1
+            S1.push(5); //5 4 3 2 1
+            S1.push(6); //Stack OverFlow
+            S1.display();
+            Console.WriteLine(S1.isEmpty());
+        }
+    }
+    class StackArray
+    {
+        int[] dataArray;
+        int top;
+        public StackArray(int size)
+        {
+            if (size <= 0)
+                size = 1;
+            dataArray = new int[size];
+            top = -1;
+        }
+        public bool isEmpty()
+        {
+            if (top == -1)
+                return true;
+            else
+                return false;
+        }
+        public bool isFull()
+        {
+            if (top == dataArray.Length - 1)
+                return true;
+            else
+                return false;
+        }
+        public void clear()
+        {
+            top = -1;
+            Console.WriteLine("Stack is empty");
+        }
+        public void push(int data)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Stack OverFlow");
+                return;
+            }
+            else
+            {
+                dataArray[++top] = data;
+            }
+        }
+        public void pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack UnderFlow");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Value popped is : " + dataArray[top--]);
+            }
+        }
+        public void peek()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack UnderFlow");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("top element is : " + dataArray[top]);
+            }
+        }
+        public void display()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack UnderFlow");
+                return;
+            }
+            else
+            {
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.Write(dataArray[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Stack Using Linked List/Stack Using Linked List/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Stack_Using_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackLinkedList S1 = new StackLinkedList();
+            S1.push(1); //1
+            S1.push(2); //2 1
+            S1.push(3); //3 2 1
+            S1.display();
+            S1.pop(); //2 1
+            S1.display();
+            S1.pop(); //1
+            S1.pop(); //empty
+            S1.pop(); //Stack UnderFlow
+        }
+    }
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    class StackLinkedList
+    {
+        Node top;
+        public void push(int data)
+        {
+            Node temp = new Node(data);
+            if (isEmpty())
+            {
+                top = temp;
+            }
+            else
+            {
+                temp.next = top;
+                top = temp;
+            }
+        }
+        public void pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack UnderFlow");
+            }
+            else
+            {
+                Console.WriteLine("Value popped is : " + top.data);
+                top = top.next;
+            }
+        }
+        public void peek()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack UnderFlow");
+            }
+            else
+            {
+                Console.WriteLine("Top node is : " + top.data);
+            }
+        }
+        public void clear()
+        {
+            top = null;
+            Console.WriteLine("Stack is empty");
+        }
+        public bool isEmpty()
+        {
+            if (top == null)
+                return true;
+            else
+                return false;
+        }
+        public void display()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack UnderFlow");
+                return;
+            }
+            Node p = top;
+            while (p != null)
+            {
+                Console.Write(p.data + " ");
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}

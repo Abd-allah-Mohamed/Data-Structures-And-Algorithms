17c300df3f0d687d56d4d17ca92d2bffe8c39996
Abdallah Mohamed <82576029+Abd-allah-Mohamed@users.noreply.github.com>
2023-05-11 10:45:34 +0300

    Add files via upload

---
+++ Queue Using Linked List/Queue Using Linked List/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Queue_Using_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueLinkedList q1 = new QueueLinkedList();
+            q1.enqueue(1); //1
+            q1.enqueue(2); //1 2
+            q1.enqueue(3); //1 2 3
+            q1.enqueue(4); //1 2 3 4
+            q1.enqueue(5); //1 2 3 4 5
+            q1.dequeue(); //2 3 4 5
+            q1.display();
+        }
+    }
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    class QueueLinkedList
+    {
+        Node front;
+        Node rear;
+        public bool isEmpty()
+        {
+            if (front == null)
+                return true;
+            else
+                return false;
+        }
+        public void enqueue(int data)
+        {
+            Node temp = new Node(data);
+            if (front == null)
+            {
+                front = temp;
+                rear = temp;
+            }
+            else
+            {
+                rear.next = temp;
+                rear = temp;
+            }
+        }
+        public void dequeue()
+        {
+            if (isEmpty())
+                Console.WriteLine("Queue UnderFlow");
+            else
+            {
+                Console.WriteLine("Value dequeued is : " + front.data);
+                front = front.next;
+            }
+        }
+        public void peek()
+        {
+            if (isEmpty())
+                Console.WriteLine("Queue UnderFlow");
+            else
+                Console.WriteLine("The peek value is : " + front.data);
+        }
+        public void display()
+        {
+            Node p = front;
+            if (isEmpty())
+                Console.WriteLine("Queue is empty");
+            else
+            {
+                while (p != null)
+                {
+                    Console.Write(p.data + " ");
+                    p = p.next;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Queue With Array/Queue With Array/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Queue_With_Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            QueueArray q = new QueueArray(3);
+            q.enqueue(2); //2
+            q.enqueue(3); //2 3
+            q.enqueue(6); //2 3 6
+            q.enqueue(5); //Queue OverFlow
+            q.display();
+            q.dequeue(); //3 6
+            q.display();
+        }
+    }
+    class QueueArray
+    {
+        int[] dataArray;
+        int front, rear;
+        public QueueArray(int size)
+        {
+            dataArray = new int[size];
+            front = rear = -1;
+        }
+        public bool isEmpty()
+        {
+            if (rear == -1 || rear == front - 1)
+                return true;
+            else
+                return false;
+        }
+        public bool isFull()
+        {
+            if (rear == dataArray.Length - 1)
+                return true;
+            else
+                return false;
+        }
+        public void enqueue(int data)
+        {
+            if (isFull())
+                Console.WriteLine("Queue OverFlow");
+            else
+            {
+                if (front == -1)
+                    front = 0;
+                dataArray[++rear] = data;
+            }
+        }
+        public void dequeue()
+        {
+            if (isEmpty())
+                Console.WriteLine("Queue UnderFlow");
+            else
+                Console.WriteLine("Value dequeued is : " + dataArray[front++]);
+        }
+        public void peek()
+        {
+            if (isEmpty())
+                Console.WriteLine("Queue UnderFlow");
+            else
+                Console.WriteLine("The peek value is : " + dataArray[front]);
+        }
+        public void display()
+        {
+            if (isEmpty())
+                Console.WriteLine("Queue is empty");
+            else
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.Write(dataArray[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
